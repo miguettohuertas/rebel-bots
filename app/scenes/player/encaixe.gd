73e0e8e2326e9ccfe191ab7641d1a3ddb1f02bfb
miguettohuertas <miguetto1314@gmail.com>
2025-08-29 18:31:30 -0300

    Revert "Drag and Drop Tela_Comandos"
    This reverts commit 6a205397bee2f37b5ac186370f76e5e172103e86.

--- app/scenes/player/encaixe.gd
+++ app/scenes/player/encaixe.gd
@@ -1,13 +1,16 @@
-extends StaticBody2D
+extends Area2D
 
+signal camando_acoplado(String)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	modulate = Color(Color.MEDIUM_PURPLE, 0.7)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Global.is_dragging:
-		visible = true
-	else:
-		visible = false
+func _process(delta: float) -> void:
+	pass
+
+func _on_area_entered(area: Area2D) -> void:
+	if (area.position == position) :
+		switch(area.get)
+	pass # Replace with function body.
