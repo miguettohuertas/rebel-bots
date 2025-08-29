6a205397bee2f37b5ac186370f76e5e172103e86
miguettohuertas <miguetto1314@gmail.com>
2025-08-29 18:19:30 -0300

    Drag and Drop Tela_Comandos
    Foi feito o Drag and Drop da tela de Comandos , ainda falta fazer bastante ajuste.

--- app/global.gd
+++ app/global.gd
@@ -1,13 +1,12 @@
-extends StaticBody2D
+extends Node
 
+var is_dragging = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	modulate = Color(Color.MEDIUM_PURPLE, 0.7)
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Global.is_dragging:
-		visible = true
-	else:
-		visible = false
+func _process(delta: float) -> void:
+	pass
